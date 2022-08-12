6a2ad76d3e17e5a0fb7abb0d232f744d018162e0
egluna-dev <egluna@live.ca>
2022-08-12 18:19:52 +1000

    Initialized project files

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import logo
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+print(logo)
+
+start_game = input(
+    "Do you want to play a game of Blackjack? Type 'y' if yes and 'n' if no:\n"
+)
